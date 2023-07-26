acdb16e06dc133fcb86b5b2ae5acfb4fdc98dc90
Olga Lanz <lnz.itrum@gmail.com>
2023-07-26 10:17:11 +0300

    create function

--- DisemvowelTrolls/src/Main.java
+++ DisemvowelTrolls/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println(convertToNotTrollString(in.nextLine()));
+    }
+    public static String convertToNotTrollString(String trollString){
+        IntStream intStream = trollString.codePoints();
+        return intStream
+                .filter(num -> num != 97 && num != 101 && num != 111 && num != 105 && num != 117)
+                .mapToObj(Character::toString)
+                .collect(Collectors.joining());
     }
 }
